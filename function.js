06949bca63cf45deb04a663d03314dddd8b5c4d1
Rafal Sokolowski <rsokolowski@rsokolowski-macbookair.roam.corp.google.com>
2015-11-23 18:59:23 +0100

    aaa

--- function.js
+++ function.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export.function = function(context, data) {
-  context.success('done22: ' + data);
-=======
 exports.function = function(context, data) {
-  context.success('done: ' + data);
->>>>>>> 796931376f9ba530e991f48413cf8df2012f987d
+  context.success('done222: ' + data);
 }
