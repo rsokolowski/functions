0574dc0d69ce3bccdcb5524207750260a086d47b
Rafal Sokolowski <rsokolowski@google.com>
2015-12-01 15:42:17 +0000

    a

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ spawn = require('child_process').spawn;
 
 exports.function = function(context, data) {
   console.log('starting function');
-  ls = spawn('ps', ['aux'], { stdio: 'inherit' });
+  ls = spawn('ls', { stdio: 'inherit' });
   ls.on('close', function (code) {
     console.log('finishing function');
     context.success('ls process exited with code ' + code);
