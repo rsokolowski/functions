9b1cac2b5abdc2c2a874d8ee6819e9c1581c2c39
Rafał Sokołowski <Rafal.Sokolowski87@gmail.com>
2015-12-21 15:26:43 +0000

    aa

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 spawn = require('child_process').spawn;
 
 exports.fooBar = function(context, data) {
-  context.success('it works blah blah ' + data);
+  context.success('it works blah blah blah blah blah ' + data);
 }
