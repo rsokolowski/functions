0413407fa06a5f7b43b1c7459978d31c9808347d
Rafal Sokolowski <rsokolowski@google.com>
2016-01-13 20:15:25 +0000

    a

--- function.js
+++ function.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-exports.function = function(context, data) {
-  console.log(data);
-  context.done();
-=======
-spawn = require('child_process').spawn;
 
-exports.fooBar = function(context, data) {
-  context.success('it works ' + data);
->>>>>>> 12402cb793c46943a3ec860c0e97c2ee02c7b2fb
+exports.function = function(context, data) {
+  console.warn(data);
+  context.done();
 }
